1f92ab7c585db9201e7e19b5f91160c4119f4623
wsalelbus-create <wsalelbus@gmail.com>
2025-12-04 23:45:14 +0000

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -14,41 +14,6 @@ const urlsToCache = [
     './leaflet.js',
     './images/icon16.png',
     './images/icon48.png',
-    './images/icon128.png',
-    './images/icon192.png',
-    './images/icon512.png',
-    './images/station_placeholder.png'
-];
-
-// Install event - cache resources
-self.addEventListener('install', (event) => {
-    event.waitUntil(
-        caches.open(CACHE_NAME)
-            .then((cache) => {
-                console.log('Opened cache');
-                return cache.addAll(urlsToCache);
-            })
-    );
-});
-
-// Fetch event - serve from cache, fallback to network
-self.addEventListener('fetch', (event) => {
-    event.respondWith(
-        caches.match(event.request)
-            .then((response) => {
-                // Cache hit - return response
-                if (response) {
-                    return response;
-                }
-                return fetch(event.request);
-            }
-            )
-    );
-});
-
-// Activate event - clean up old caches
-self.addEventListener('activate', (event) => {
-    const cacheWhitelist = [CACHE_NAME];
     event.waitUntil(
         caches.keys().then((cacheNames) => {
             return Promise.all(
